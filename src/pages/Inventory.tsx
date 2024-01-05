fc7a12d0c766efd00c5c6a7731ed790cecabc8b7
jkmoore <jkm9@pdx.edu>
2024-01-05 21:00:26 +0900

    feat: add item search function

--- src/pages/Inventory.tsx
+++ src/pages/Inventory.tsx
@@ -19,10 +19,35 @@ import InventoryModal from "../components/InventoryModal";
 import Item from "../components/item";
 import styled from "styled-components";
 
+const StyledHeader = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 0rem;
+  margin: 0.5rem 0;
+`;
+
+const SearchBar = styled.input`
+  padding: 0.625rem;
+  border: 1px solid #ccc;
+  border-radius: 0.5rem;
+  outline: none;
+  width: 100%;
+  margin-left: 0.5rem;
+  box-shadow: 0.13rem 0.13rem 0.25rem rgba(0, 0, 0, 0.2);
+  font-size: 1rem;
+
+  ::placeholder {
+    color: #ccc;
+  }
+
+  &:focus {
+    outline: 0.1rem solid #ccc;
+  }
+`;
+
 const AddItemButton = styled.img`
   height: 2rem;
   width: 2rem;
-  margin: 0.5rem 0;
   cursor: pointer;
   border-radius: 50%;
   box-shadow: 0.13rem 0.13rem 0.25rem rgba(0, 0, 0, 0.2);
@@ -80,6 +105,8 @@ export default function Inventory() {
   const [modalMode, setModalMode] = useState<ModalMode>("add");
   const [itemToEdit, setItemToEdit] = useState<string | null>(null);
   const [selectedItemData, setSelectedItemData] = useState<Item | null>(null);
+  const [searchResults, setSearchResults] = useState<DocumentData[]>([]);
+  const [isSearching, setIsSearching] = useState<boolean>(false);
 
   const user: User | null = auth.currentUser;
 
@@ -92,31 +119,30 @@ export default function Inventory() {
   const handleSubmitItem = (newItem: Item) => {
     if (modalMode === "add") {
       handleAddItem(newItem);
-    }
-    else {
+    } else {
       setSelectedItemData(null);
       handleEditItem(newItem);
     }
-  }
+  };
 
   const handleAddItem = async (newItem: Item) => {
     addDoc(collection(firestore, "inventory"), {
       ...newItem,
       order: maxOrder + 1,
       userId: user?.uid,
-    })
-      .catch((error) => {
-        console.error("Error adding item:", error);
-      });
+    }).catch((error) => {
+      console.error("Error adding item:", error);
+    });
   };
 
   const handleEditItem = async (newItem: Item) => {
     try {
       if (itemToEdit) {
-        await updateDoc(doc(firestore, "inventory", itemToEdit), { ...newItem });
+        await updateDoc(doc(firestore, "inventory", itemToEdit), {
+          ...newItem,
+        });
         setItemToEdit(null);
-      }
-      else {
+      } else {
         console.error("Error updating item: item id is null");
       }
     } catch (error) {
@@ -140,13 +166,47 @@ export default function Inventory() {
     setModalMode("edit");
     setItemToEdit(item.id);
     if (item.expiration) {
-      setSelectedItemData({ name: item.name, expiration: item.expiration.toDate() });
-    }
-    else {
+      setSelectedItemData({
+        name: item.name,
+        expiration: item.expiration.toDate(),
+      });
+    } else {
       setSelectedItemData({ name: item.name, expiration: null });
     }
     setShowModal(true);
-  }
+  };
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const queryInputValue = event.target.value.trim();
+
+    if (queryInputValue) {
+      setIsSearching(true);
+      const searchRef = query(
+        collection(firestore, "inventory"),
+        where("userId", "==", user?.uid),
+        where("name", ">=", queryInputValue),
+        where("name", "<=", queryInputValue + "\uf8ff")
+      );
+
+      onSnapshot(
+        searchRef,
+        (querySnapshot: QuerySnapshot<DocumentData>) => {
+          const results: DocumentData[] = [];
+          querySnapshot.forEach((doc: QueryDocumentSnapshot<DocumentData>) => {
+            const result = { id: doc.id, ...doc.data() };
+            results.push(result);
+          });
+          setSearchResults(results);
+        },
+        (error) => {
+          console.error("Error searching inventory:", error);
+        }
+      );
+    } else {
+      setIsSearching(false);
+      setSearchResults([]);
+    }
+  };
 
   useEffect(() => {
     const inventoryRef = query(
@@ -183,34 +243,77 @@ export default function Inventory() {
 
   return (
     <StyledDiv>
-      <AddItemButton
-        src={process.env.PUBLIC_URL + "/buttonAddItem.svg"}
-        alt="Add Item"
-        onClick={() => { setModalMode("add"); setShowModal(true); }}
-      />
-      {showModal && <InventoryModal setIsOpen={setShowModal} onSubmitItem={handleSubmitItem} defaultData={selectedItemData} mode={modalMode} />}
+      <StyledHeader>
+        <AddItemButton
+          src={process.env.PUBLIC_URL + "/buttonAddItem.svg"}
+          alt="Add Item"
+          onClick={() => {
+            setModalMode("add");
+            setShowModal(true);
+          }}
+        />
+        <SearchBar
+          placeholder="Search for an item"
+          onChange={handleSearchChange}
+        />
+      </StyledHeader>
+      {showModal && (
+        <InventoryModal
+          setIsOpen={setShowModal}
+          onSubmitItem={handleSubmitItem}
+          defaultData={selectedItemData}
+          mode={modalMode}
+        />
+      )}
       {loading ? (
         <p>Loading...</p>
-      ) : inventory.length === 0 ? (
-        <p>No items in the inventory.</p>
+      ) : isSearching && searchResults.length === 0 ? (
+        <p>No matching items found.</p>
       ) : (
         <StyledList>
-          {inventory.map((item) => (
-            <StyledListItem key={item.id} onClick={() => handleClickItem(item)}>
-              <DeleteItemButton
-                src={process.env.PUBLIC_URL + "/buttonCheckItem.svg"}
-                alt="Remove Item"
-                onClick={(e) => { e.stopPropagation(); handleDeleteItem(item.id); }}
-              />
-              {item.name}
-              {item.expiration && (
-                <StyledSpan>
-                  {item.expiration.toDate().toISOString().slice(0, 10)}
-                </StyledSpan>
-              )}
-
-            </StyledListItem>
-          ))}
+          {isSearching
+            ? searchResults.map((item) => (
+                <StyledListItem
+                  key={item.id}
+                  onClick={() => handleClickItem(item)}
+                >
+                  <DeleteItemButton
+                    src={process.env.PUBLIC_URL + "/buttonCheckItem.svg"}
+                    alt="Remove Item"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      handleDeleteItem(item.id);
+                    }}
+                  />
+                  {item.name}
+                  {item.expiration && (
+                    <StyledSpan>
+                      {item.expiration.toDate().toISOString().slice(0, 10)}
+                    </StyledSpan>
+                  )}
+                </StyledListItem>
+              ))
+            : inventory.map((item) => (
+                <StyledListItem
+                  key={item.id}
+                  onClick={() => handleClickItem(item)}
+                >
+                  <DeleteItemButton
+                    src={process.env.PUBLIC_URL + "/buttonCheckItem.svg"}
+                    alt="Remove Item"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      handleDeleteItem(item.id);
+                    }}
+                  />
+                  {item.name}
+                  {item.expiration && (
+                    <StyledSpan>
+                      {item.expiration.toDate().toISOString().slice(0, 10)}
+                    </StyledSpan>
+                  )}
+                </StyledListItem>
+              ))}
         </StyledList>
       )}
     </StyledDiv>
