82c43435c5bd04e6fd0750afff572d35fbeb9784
jkmoore <jkm9@pdx.edu>
2024-06-29 07:02:17 +0900

    feat: hide firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCnaH-6ndEOBUJIIwQKkPUzXP5odtanidY",
-  authDomain: "whats-for-dinner-26f01.firebaseapp.com",
-  projectId: "whats-for-dinner-26f01",
-  storageBucket: "whats-for-dinner-26f01.appspot.com",
-  messagingSenderId: "836493515880",
-  appId: "1:836493515880:web:20e325c200e91738502fdd",
-  measurementId: "G-BQ6FM4B466",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase and Firebase Authentication
