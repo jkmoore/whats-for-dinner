78acfb0cf46573a3480b5cd845b6607dd641af29
jkmoore <jkm9@pdx.edu>
2024-07-04 14:48:36 +0900

    feat: add links to not found page

--- src/components/pages/NotFound/NotFound.tsx
+++ src/components/pages/NotFound/NotFound.tsx
@@ -1,5 +1,23 @@
 import React from "react";
+import { auth } from "../../../firebase";
+import { Link } from "react-router-dom";
+import { User } from "firebase/auth";
 
 export default function NotFound() {
-  return <h1>This page was not found.</h1>;
+  const user: User | null = auth.currentUser;
+
+  return (
+    <div>
+      <h1>This page was not found.</h1>
+      <p>
+        {user ? (
+          <Link to="/inventory">Return to inventory</Link>
+        ) : (
+          <>
+            <Link to="/login">Login</Link> or <Link to="/signup">Signup</Link>
+          </>
+        )}
+      </p>
+    </div>
+  );
 }
